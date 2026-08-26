ed17c29133452e75e4df468034458288b2fea5b1
nayan chandra shil <shantomozumder33@gmail.com>
2026-08-26 19:46:39 +0600

    feat: add course, lesson, quiz, question, enrollment, progress, quizresult content-types with relations
    created course, lesson, quiz, question, enrollment, progress, quizresult content-types with fields and relations.

---
+++ server/src/api/quizresult/controllers/quizresult.ts
@@ -0,0 +1,7 @@
+/**
+ * quizresult controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::quizresult.quizresult');
---
+++ server/src/api/quizresult/routes/quizresult.ts
@@ -0,0 +1,7 @@
+/**
+ * quizresult router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::quizresult.quizresult');
